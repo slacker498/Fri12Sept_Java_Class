b87d93ff3b03079f9790c89d451ec274b6028f36
slacker498 <jachinkpogli@gmail.com>
2025-09-12 15:19:01 +0000

    modified java file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        String nameStd = sc.nextLine();
+        int age = sc.nextInt();
+        sc.close();
+
     }
 }
